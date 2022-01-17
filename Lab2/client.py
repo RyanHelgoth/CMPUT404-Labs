2f9ceb3f456af01c91c15cb94395585ece869fad
RyanHelgoth <78457606+RyanHelgoth@users.noreply.github.com>
2022-01-17 14:55:54 -0700

    L2: added client.py

---
+++ Lab2/client.py
@@ -0,0 +1,30 @@
+#Written by Ryan Helgoth 
+
+#TODO: finish citations, test on vm, check if encode is needed on vm
+
+import socket
+
+def main():
+    #https://pythonprogramminglanguage.com/socket-client/
+    #https://www.geeksforgeeks.org/socket-programming-python/
+    host = "www.google.com"
+    port = 80
+
+    #AF_INET specifies we are using IPV4, SOCK_STREAM specifies we are using TCP
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip = socket.gethostbyname(host)
+
+    mySocket.connect((ip, port))
+
+    #https://stackoverflow.com/a/818188
+    request = "GET / HTTP/1.1\r\n\r\n".encode(encoding = "us_ascii")
+    
+    mySocket.sendall(request)
+
+    bufferSize = 4096 #Max amount of bytes to receive at once
+    reply = mySocket.recv(bufferSize)
+
+    print(reply)
+
+
+main() 
